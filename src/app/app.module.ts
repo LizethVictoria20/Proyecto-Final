3641742689b75bfef2b74d2807d501fbaab86de0
Lizeth Victoria Franco <lizvictoria754@gmail.com>
2018-12-04 13:27:48 -0500

    Agregando componentes y estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { ButtonComponent } from './button/button.component';
+import { RazasComponent } from './razas/razas.component';
+import { FooterComponent } from './footer/footer.component';
+import { InformacionComponent } from './informacion/informacion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    ButtonComponent
+    ButtonComponent,
+    RazasComponent,
+    FooterComponent,
+    InformacionComponent
   ],
   imports: [
     BrowserModule,
