8ac2f2bf297a18b5de0fd5a40cdd1cc9717738bc
Lizeth Victoria Franco <lizvictoria754@gmail.com>
2018-12-06 15:18:01 -0500

    Cambios con todas las ramas mezcladas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { ButtonComponent } from './button/button.component';
+import { RazasComponent } from './razas/razas.component';
+import { FooterComponent } from './footer/footer.component';
+import { BotoncamilaComponent } from 'src/app/botoncamila/botoncamila.component';
+import { MenuComponent } from 'src/app/menu/menu.component';
+import { NavbarComponent } from 'src/app/navbar/navbar.component';
 
 
 
