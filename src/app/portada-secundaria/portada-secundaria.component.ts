5822bb02e3357b1b194902a4f8e9e9b35728ad70
Lizeth Victoria Franco <lizvictoria754@gmail.com>
2018-12-11 14:47:22 -0500

    Portada para cada seccion

---
+++ src/app/portada-secundaria/portada-secundaria.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portada-secundaria',
+  templateUrl: './portada-secundaria.component.html',
+  styleUrls: ['./portada-secundaria.component.css']
+})
+export class PortadaSecundariaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
